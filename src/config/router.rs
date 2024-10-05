9e1b7f5b7a1a7ce1c6dc5b75776d4dd664c775a6
reigadegr <2722688642@qq.com>
2024-10-05 15:38:15 +0800

    添加用户登录

--- src/config/router.rs
+++ src/config/router.rs
@@ -1,8 +1,10 @@
 use salvo::Router;
 use crate::research::login::get_user;
 use crate::research::login::get_user2;
+use crate::research::login::user_login;
+
 pub fn init_router() -> Router {
     Router::new()
         .push(Router::with_path("/users").get(get_user))
-        .push(Router::with_path("/ty").get(get_user2))
+        .push(Router::with_path("/login").get(user_login))
 }
--- src/research/login.rs
+++ src/research/login.rs
@@ -2,19 +2,21 @@ use salvo::{handler, Request, Response};
 use crate::{RB, Users};
 
 impl_select!(Users{select_by_id(id:String) -> Option => "`where id = #{id} limit 1`"});
-impl_select!(Users{select_by_type(phone:String) -> Option => "`where phone = #{phone} limit 1`"});
+impl_select!(Users{login(username:String,password:String) -> Option => "`where username = #{username} AND password = #{password} limit 1`"});
+
+#[handler]
+pub async fn user_login(req: &mut Request, res: &mut Response) {
+    //示例：http://127.0.0.1:5800/login/?username=admin&password=123456
+    let username = req.query::<&str>("username").unwrap();
+    let password = req.query::<&str>("password").unwrap();
+    let data = Users::login(&mut RB.clone(), username.to_string(),password.to_string()).await.unwrap();
+    println!("{:?}", data);
+    res.render(serde_json::to_string(&data).unwrap());
+}
 #[handler]
 pub async fn get_user(req: &mut Request, res: &mut Response) {
     let uid = req.query::<i8>("id").unwrap();
     let data = Users::select_by_id(&mut RB.clone(), uid.to_string()).await.unwrap();
-    println!("{:?}", data);
-    res.render(serde_json::to_string(&data).unwrap());
-}
-
-#[handler]
-pub async fn get_user2(req: &mut Request, res: &mut Response) {
-    let uid = req.query::<&str>("phone").unwrap();
-    let data = Users::select_by_type(&mut RB.clone(), uid.to_string()).await.unwrap();
     println!("{:?}", data);
     res.render(serde_json::to_string(&data).unwrap());
 }
