f341b0be6488f51351e3928e38d2f1921ba16732
reigadegr <272268642@qq.com>
2024-10-16 23:45:09 +0800

    action: Add action to CI

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 if [ "$1" == "android" ]; then
     cargo +nightly ndk -p 34 -t arm64-v8a build --target aarch64-linux-android -r
+    dd if=./target/aarch64-linux-android/release/salvo-rbatis-demo of=./module/salvo-rbatis-demo-android
     return
 fi
-
-cargo build
